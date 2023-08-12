537679fb400f4d5b648a30fdf2c0321c5d1236e9
Sergeii Samus <cergi8273@gmail.com>
2023-08-12 22:01:41 +0300

    create token and fix registration and login

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,60 +1,51 @@
-
-import axios, { AxiosResponse } from 'axios';
-import { commercetoolsConfig } from '../commercetoolsConfig';
+import axios, { AxiosResponse } from 'axios'
+import { commercetoolsConfig } from '../commercetoolsConfig'
+import apiClient from '../api/axios'
 
 interface AuthResponseData {
-  token: string;
-  user: string;
+  token: string
+  user: string
 }
 
-export const authenticateUser = async (
-  email: string,
-  password: string
-): Promise<boolean> => {
+export const registerUser = async (email: string, password: string): Promise<boolean> => {
   try {
-    const response: AxiosResponse<AuthResponseData> = await axios.post(
-      `${commercetoolsConfig.host}/oauth/${commercetoolsConfig.projectKey}/customers/token`,
+    const response: AxiosResponse<AuthResponseData> = await apiClient.post(
+      `/${commercetoolsConfig.projectKey}/customers`,
       {
         email,
         password,
       }
-    );
+    )
 
     if (response.status === 201) {
-      const token = response.data.token;
-      const user = response.data.user;
-      console.log(token, user);
+      const token = response.data.token
+      const user = response.data.user
+      console.log(token, user)
       // Сохранение токена и данных пользователя в состоянии/хранилище.
 
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
-export const registerUser = async (
-  email: string,
-  password: string
-): Promise<boolean> => {
+export const authenticateUser = async (email: string, password: string): Promise<boolean> => {
   try {
-    const response: AxiosResponse<any> = await axios.post(
-      `${commercetoolsConfig.host}/api/register`, 
-      {
-        email,
-        password,
-      }
-    );
+    const response: AxiosResponse<any> = await axios.post(`${commercetoolsConfig.host}/api/register`, {
+      email,
+      password,
+    })
 
     if (response.status === 200) {
       // Регистрация прошла успешно.
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
