c658503a686eea4129e288c9b0dd143ed37cf0bc
PopovaOlha <fc.odess.10@gmail.com>
2023-08-23 16:51:09 +0300

    feat: reloading page

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
+window.addEventListener('beforeunload', function () {
+  localStorage.clear()
+})
