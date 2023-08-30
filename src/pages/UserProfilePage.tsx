1c5fd9a98a0df67bf4a07891dfa3b44e3b396968
Anastasiia Dedela <nastyadedela@gmail.com>
2023-08-30 22:05:32 +0200

    feat: add editing mode for basic user info

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -3,7 +3,11 @@ import { observer } from 'mobx-react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import UserImg from '../assets/user.png'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import axios from '../api/axios'
 import '../styles/user-profile.scss'
+import { projectKey } from '../commercetoolsConfig'
 
 interface Address {
   city: string
@@ -14,6 +18,7 @@ interface Address {
 }
 
 interface User {
+  id: string
   firstName: string
   lastName: string
   dateOfBirth: string
@@ -23,6 +28,7 @@ interface User {
 
 const UserProfilePage = () => {
   const [user, setUser] = useState<User | null>(null)
+  const [editState, setEditState] = useState(false)
 
   useEffect(() => {
     const { customer } = JSON.parse(localStorage.getItem('user')!)
@@ -30,6 +36,52 @@ const UserProfilePage = () => {
     setUser(customer)
     console.log('userData', customer)
   }, [])
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .matches(/^[A-Za-zА-Яа-я\s]+$/, 'Last name must contain only alphabetic characters')
+      .test('no-spaces-around', 'Last name cannot start or end with a space', (value) => {
+        if (value) {
+          return !/^\s|\s$/.test(value)
+        }
+        return true
+      }),
+    lastName: Yup.string()
+      .matches(/^[A-Za-zА-Яа-я\s]+$/, 'Last name must contain only alphabetic characters')
+      .test('no-spaces-around', 'Last name cannot start or end with a space', (value) => {
+        if (value) {
+          return !/^\s|\s$/.test(value)
+        }
+        return true
+      }),
+    dateOfBirth: Yup.date(),
+    email: Yup.string().email(),
+  })
+
+  const handleEditProfile = () => {
+    setEditState(!editState)
+  }
+
+  const handleUserProfileSubmit = (values: {
+    firstName: string
+    lastName: string
+    dateOfBirth: string
+    email: string
+  }) => {
+    const payload = {
+      version: '3',
+      actions: [
+        {
+          action: 'updateCustomer',
+          ...values,
+        },
+      ],
+    }
+    axios.post(`/${projectKey}/customers/${user?.id}`, payload).then((res) => {
+      console.log('Res: ', res)
+    })
+  }
+
   return (
     <div>
       <Header />
@@ -42,27 +94,91 @@ const UserProfilePage = () => {
           </div>
 
           <div className="user__data">
-            <div className="user-data__list">
-              <div className="user-data__item">
-                <p className="title">Name:</p> <p>{user && user?.firstName}</p>
-              </div>
-              <div className="user-data__item">
-                <p className="title">Last Name:</p> <p>{user && user?.lastName}</p>
-              </div>
-              <div className="user-data__item">
-                <p className="title">Date of birth:</p>{' '}
-                {/* <p>{user && user?.dateOfBirth.split('-').reverse().join('-')}</p> */}
-                {/* Here we still don't have dateOfBirth in the customer object which we get from commercetools*/}
-                <p>{user && user?.dateOfBirth}</p>
-              </div>
-              <div className="user-data__item">
-                <p className="title">E-mail:</p> <p>{user && user?.email}</p>
-              </div>
-              <div className="profile-buttons">
-                <button className="profile-button">Edit profile</button>
-                <button className="profile-button">Change password</button>
-              </div>
-            </div>
+            {user && (
+              <Formik
+                initialValues={{
+                  firstName: user?.firstName,
+                  lastName: user?.lastName,
+                  dateOfBirth: user?.dateOfBirth,
+                  email: user?.email,
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleUserProfileSubmit}
+              >
+                {(formik) => (
+                  <div className="user-data__list">
+                    <div className="user-data__item">
+                      <p className="title">Name:</p>
+                      {formik.errors.firstName && <p color="red">Error: {formik.errors.firstName}</p>}
+                      {editState ? (
+                        <input
+                          type="text"
+                          name="firstName"
+                          value={formik.values.firstName}
+                          onChange={formik.handleChange}
+                        />
+                      ) : (
+                        <p>{user?.firstName}</p>
+                      )}
+                    </div>
+                    <div className="user-data__item">
+                      <p className="title">Last Name:</p>
+                      {formik.errors.lastName && <p color="red">Error: {formik.errors.lastName}</p>}
+                      {editState ? (
+                        <input
+                          type="text"
+                          name="lastName"
+                          value={formik.values.lastName}
+                          onChange={formik.handleChange}
+                        />
+                      ) : (
+                        <p>{user?.lastName}</p>
+                      )}
+                    </div>
+                    <div className="user-data__item">
+                      <p className="title">Date of birth: </p>
+                      {formik.errors.dateOfBirth && <p color="red">Error: {formik.errors.dateOfBirth}</p>}
+                      {editState ? (
+                        <input
+                          type="text"
+                          name="dateOfBirth"
+                          value={formik.values.dateOfBirth}
+                          onChange={formik.handleChange}
+                        />
+                      ) : (
+                        <p>{user?.dateOfBirth}</p>
+                      )}
+                    </div>
+                    <div className="user-data__item">
+                      <p className="title">E-mail:</p>
+                      {formik.errors.email && <p color="red">Error: {formik.errors.email}</p>}
+                      {editState ? (
+                        <input type="text" name="email" value={formik.values.email} onChange={formik.handleChange} />
+                      ) : (
+                        <p>{user?.email}</p>
+                      )}
+                    </div>
+                    <div className="profile-buttons">
+                      {editState && (
+                        <button className="profile-button save-button" disabled={!formik.isValid}>
+                          Save Changes
+                        </button>
+                      )}
+                      {editState ? (
+                        <button className="profile-button cancel-button" onClick={handleEditProfile}>
+                          Cancel
+                        </button>
+                      ) : (
+                        <button className="profile-button" onClick={handleEditProfile}>
+                          Edit profile
+                        </button>
+                      )}
+                      {!editState && <button className="profile-button">Change password</button>}
+                    </div>
+                  </div>
+                )}
+              </Formik>
+            )}
           </div>
         </div>
         <div className="addresses">
