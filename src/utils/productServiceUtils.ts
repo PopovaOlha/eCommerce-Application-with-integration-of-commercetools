eda808e28592a960e4cc1c740d0ce195e4b6e6c2
PopovaOlha <fc.odess.10@gmail.com>
2023-09-02 21:19:01 +0300

    feat: add discount price to the ProductPage component

--- src/utils/productServiceUtils.ts
+++ src/utils/productServiceUtils.ts
@@ -7,23 +7,30 @@ const apiUrl = 'https://api.europe-west1.gcp.commercetools.com';
 export async function fetchProducts(): Promise<Product[]> {
   try {
     const authDataString = localStorage.getItem('authData');
-      const token = JSON.parse(authDataString!);
-    const response = await axios.get<{ results: RawProduct[]}>(
+    const token = JSON.parse(authDataString!);
+    const response = await axios.get<{ results: RawProduct[] }>(
       `${apiUrl}/${commercetoolsConfig.projectKey}/products`,
       {
         headers: {
           Authorization: `Bearer ${token.accessToken}`,
         },
-  });
+      }
+    );
 
-    return response.data.results.map((rawProduct) => ({
-      id: rawProduct.id,
-      key: rawProduct.key,
-      name: rawProduct.masterData.current.name,
-      description: rawProduct.masterData.staged.description || 'No description available',
-      imageUrl: rawProduct.masterData.current.masterVariant.images.map(image => image.url),
-      price: rawProduct.masterData.current.masterVariant.prices.map(price => price.value.centAmount),
-    }));
+    return response.data.results.map((rawProduct) => {
+      const discountedPrice = rawProduct.masterData.current.masterVariant.prices.find(price => price.discounted);
+      const discount = discountedPrice ? discountedPrice.discounted!.value.centAmount : null;
+
+      return {
+        id: rawProduct.id,
+        key: rawProduct.key,
+        name: rawProduct.masterData.current.name,
+        description: rawProduct.masterData.staged.description || 'No description available',
+        imageUrl: rawProduct.masterData.current.masterVariant.images.map(image => image.url),
+        price: rawProduct.masterData.current.masterVariant.prices.map(price => price.value.centAmount),
+        discount: discount,
+      };
+    });
 
   } catch (error) {
     console.error('Error fetching products:', error);
