f160764ec29d1d66c962702c87c607665bcf5ff2
PopovaOlha <fc.odess.10@gmail.com>
2023-09-06 19:08:15 +0300

    fix:image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ import '../index.css'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-import backgroundImage from '../images/1660766295_41-kartinkin-net-p-fon-dlya-zootovarov-krasivo-45.png'
+import backgroundImageDesktop from '../images/1660766295_41-kartinkin-net-p-fon-dlya-zootovarov-krasivo-45.png'
+import backgroundImageMobile from '../images/1671769944_kalix-club-p-fon-domashnie.jpg'
 
 function Home() {
   const backgroundStyle = {
@@ -13,17 +14,23 @@ function Home() {
     backgroundPosition: 'center',
   }
 
-  const mobileBackgroundStyle = {
+  const desktopBackgroundStyle = {
     ...backgroundStyle,
-    backgroundImage: `url(${backgroundImage})`,
+    backgroundImage: `url(${backgroundImageDesktop})`, // Desktop background image
   }
 
+  const mobileBackgroundStyle = {
+    ...backgroundStyle,
+    backgroundImage: `url(${backgroundImageMobile})`, // Mobile background image
+  }
+
+  // Define a media query for mobile screens (adjust max-width as needed)
   const isMobile = window.matchMedia('(max-width: 768px)').matches
 
   return (
     <div>
       <Header subcategories={[]} />
-      <div style={isMobile ? mobileBackgroundStyle : backgroundStyle} className="content"></div>
+      <div style={isMobile ? mobileBackgroundStyle : desktopBackgroundStyle} className="content"></div>
       <Footer />
     </div>
   )
