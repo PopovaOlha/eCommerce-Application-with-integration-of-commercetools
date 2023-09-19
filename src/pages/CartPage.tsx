e32ba1d31f81514473d2a351c30cf2bde333df3b
PopovaOlha <fc.odess.10@gmail.com>
2023-09-19 16:31:16 +0300

    feat: add promocod functionality

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,196 +1,234 @@
-import { makeAutoObservable } from 'mobx'
-import api from '../api/axios'
-import { commercetoolsConfig } from '../commercetoolsConfig'
-import { AxiosResponse } from 'axios'
-// import { useRootStore } from '../App'
-import { CartItem, LineItem } from '../types/interfaces'
+import { observer } from 'mobx-react-lite'
+import Header from '../components/Header'
+import '../index.css'
+import { useRootStore } from '../App'
+import { Product, CartItem } from '../types/interfaces'
+import { Container, Card, CardContent, CardActions, Typography, IconButton, Grid, Button } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+import { Carousel } from 'react-responsive-carousel'
+import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
-interface ApiResponse {
-  id: string
-  name: string
-  lineItems: LineItem[]
-  version: number
+const CartPage = () => {
+  const { cartStore, catalogStore, headerStore } = useRootStore()
+  const [cartItemsLocal, setCartItemsLocal] = useState<CartItem[]>([])
+
+  /*const [promoCodeInput, setPromoCodeInput] = useState('')
+  const DataPromoCode = localStorage.getItem('promoCode')
+  const promoCode = JSON.parse(DataPromoCode!)
+  const [activePromoCodes, setActivePromoCodes] = useState<string[]>(promoCode)
+  console.log(setActivePromoCodes)
+
+  const handleApplyPromoCode = () => {
+    if (promoCodeInput && activePromoCodes.includes(promoCodeInput)) {
+      cartStore.applyPromoCode(promoCodeInput)
+      setPromoCodeInput('')
+    } else {
+      alert('Недействительный промокод')
+    }
+  }*/
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await cartStore.createCart()
+      const cartId: string = localStorage.getItem('cartId')!
+      await cartStore.getCurrentCartState(cartId)
+
+      const items = localStorage.getItem('cartItem')!
+      const parsedItems: CartItem[] = JSON.parse(items)
+      setCartItemsLocal(parsedItems)
+    }
+
+    fetchData()
+  }, [cartStore])
+
+  const calculateTotalPrice = () => {
+    let total = 0
+    cartItemsLocal.forEach((item) => {
+      const product: Product | undefined = catalogStore.getProductById(item.productId)
+      if (product) {
+        total += (product.price[0] * item.quantity) / 100
+      }
+    })
+    return total
+  }
+  const handleRemoveFromCart = async (productId: string) => {
+    await cartStore.removeFromCart(productId)
+
+    // Обновляем cartItemsLocal без использования флага
+    const updatedCartItems = cartItemsLocal.filter((item) => item.productId !== productId)
+    setCartItemsLocal(updatedCartItems)
+    const cartItem = JSON.parse(localStorage.getItem('cartItem')!)
+    console.log(cartItem)
+
+    headerStore.decrementCartCount()
+  }
+  const handleupdateCartItemQuantity = async (productId: string, quantity: number) => {
+    try {
+      const cartItemToUpdate = cartItemsLocal.find((item) => item.productId === productId)
+
+      if (!cartItemToUpdate) {
+        console.error('Товар не найден в корзине.')
+        return
+      }
+
+      let updatedCartItem: CartItem
+
+      if (typeof cartItemToUpdate.discountPrice === 'number') {
+        updatedCartItem = {
+          ...cartItemToUpdate,
+          quantity: quantity,
+          totalPrice: cartItemToUpdate.discountPrice * quantity,
+        }
+      } else {
+        updatedCartItem = {
+          ...cartItemToUpdate,
+          quantity: quantity,
+          totalPrice: cartItemToUpdate.price * quantity,
+        }
+      }
+
+      // Обновляем корзину на сервере
+      await cartStore.updateCartItemQuantity(productId, quantity)
+
+      // Проверяем, если количество товара меньше или равно нулю, то удаляем товар из массива
+      if (quantity <= 0) {
+        const updatedCartItems = cartItemsLocal.filter((item) => item.productId !== productId)
+        setCartItemsLocal(updatedCartItems)
+        headerStore.decrementCartCount()
+      } else {
+        // В противном случае, обновляем cartItemsLocal с обновленными данными
+        const updatedCartItems = cartItemsLocal.map((item) => {
+          if (item.productId === productId) {
+            return updatedCartItem
+          }
+          return item
+        })
+
+        setCartItemsLocal(updatedCartItems)
+      }
+    } catch (error) {
+      console.error('Произошла ошибка при обновлении количества товара в корзине:', error)
+    }
+  }
+  const hasItemsInCart = cartItemsLocal!.some((item) => item !== null)
+  console.log('sadasda')
+  const cartItems = cartItemsLocal.map((item) => {
+    const product: Product | undefined = catalogStore.getProductById(item.productId)
+    console.log(product)
+    if (product) {
+      return (
+        <>
+          <Card key={item.productId} className="cart-item">
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={3}>
+                  <Carousel showThumbs={false} dynamicHeight>
+                    {product.imageUrl.map((image, index) => (
+                      <img
+                        key={index}
+                        src={image}
+                        alt={`Product Image ${index}`}
+                        className="product-image"
+                        style={{ width: '100%', maxWidth: '300px', height: '150px' }}
+                      />
+                    ))}
+                  </Carousel>
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                  <Typography variant="h6" gutterBottom>
+                    {product.name['en-US']}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" gutterBottom>
+                    Quantity: {item.quantity}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" gutterBottom>
+                    Price per unit: ${(item.price / 100).toFixed(2)}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" gutterBottom>
+                    Discount Price:{' '}
+                    {typeof item.discountPrice === 'string'
+                      ? item.discountPrice
+                      : (item.discountPrice / 100).toFixed(2)}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" gutterBottom>
+                    Total Price: ${(item.totalPrice / 100).toFixed(2)}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+            <CardActions>
+              <IconButton aria-label="Remove" color="error" onClick={() => handleRemoveFromCart(item.productId)}>
+                <DeleteIcon />
+              </IconButton>
+              <IconButton
+                aria-label="Increase Quantity"
+                onClick={() => handleupdateCartItemQuantity(item.productId, item.quantity + 1)}
+              >
+                <AddIcon />
+              </IconButton>
+              <IconButton
+                aria-label="Decrease Quantity"
+                onClick={() => handleupdateCartItemQuantity(item.productId, item.quantity - 1)}
+              >
+                <RemoveIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </>
+      )
+    } else {
+      return null
+    }
+  })
+  console.log(cartItems)
+  console.log(hasItemsInCart)
+  return (
+    <Container>
+      <Header subcategories={[]} />
+      <Grid container spacing={2} className="cart-container">
+        <div
+          style={{
+            display: 'inherit',
+            margin: '50px auto auto auto',
+            marginTop: '50px',
+          }}
+        >
+          <div style={{ padding: '20px 40px' }}>
+            <Button variant="outlined" color="primary">
+              <Link to="/catalog">CONTINUE SHOPPING</Link>
+            </Button>
+            {/*<div>
+            <input
+              type="text"
+              placeholder="Enter promotional code"
+              value={promoCodeInput}
+              onChange={(e) => setPromoCodeInput(e.target.value)}
+              className="cart-input"
+            />
+            <button onClick={handleApplyPromoCode}>Apply</button>
+        </div>*/}
+          </div>
+          {hasItemsInCart && (
+            <div className="cart-buttons">
+              <div>Total Price: ${calculateTotalPrice().toFixed(2)}</div>
+              <Button variant="contained" color="primary">
+                Сheckout
+              </Button>
+            </div>
+          )}
+        </div>
+        {cartItems}
+      </Grid>
+
+      <div style={{ position: 'fixed', bottom: '0', left: '0', width: '100%' }}>
+        <Footer />
+      </div>
+    </Container>
+  )
 }
 
-class CartStore {
-  getActivePromoCodes() {
-    throw new Error('Method not implemented.')
-  }
-  isLoading = false
-  cartItems: CartItem[] = []
-  cartId!: string
-
-  constructor() {
-    makeAutoObservable(this)
-  }
-
-  async getCart(): Promise<string> {
-    try {
-      const response: AxiosResponse = await api.get(
-        `${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts`
-      )
-      console.log()
-      return response.data.results[response.data.results.length - 1].id
-    } catch (error) {
-      const response: AxiosResponse = await api.get(
-        `${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts`
-      )
-      console.log()
-      return response.data.results[response.data.results.length - 1].id
-    }
-  }
-  async createCart() {
-    try {
-      this.isLoading = true
-      this.cartId = await this.getCart()
-      this.isLoading = false
-      localStorage.setItem('cartId', this.cartId)
-    } catch (error) {
-      this.isLoading = false
-      const requestData = {
-        country: 'US',
-        currency: 'USD',
-      }
-      const response = await api.post(
-        `${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts`,
-        requestData
-      )
-      this.cartId = response.data.id
-      localStorage.setItem('cartId', response.data.id)
-    }
-  }
-
-  async addToCart(productId: string): Promise<void> {
-    try {
-      this.isLoading = true
-
-      const cartId: string = localStorage.getItem('cartId')!
-
-      const currentCartState = await this.getCurrentCartState(cartId)
-
-      const requestData = {
-        version: currentCartState.version,
-        actions: [
-          {
-            action: 'addLineItem',
-            productId: productId,
-            quantity: 1,
-          },
-        ],
-      }
-
-      const response: AxiosResponse<ApiResponse> = await api.post(
-        `${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts/${cartId}`,
-        requestData
-      )
-
-      console.log('Товар успешно добавлен в корзину:', response.data)
-
-      // const lineItem = response.data.lineItems.find((item) => item.productId === productId)
-      // // if (localStorage.getItem('cartItem') !== null) {
-      // //   const items = localStorage.getItem('cartItem')!
-      // //   const cartItemsLocal: CartItem[] = JSON.parse(items)
-      // //   this.cartItems = cartItemsLocal
-      // // }
-      // if (lineItem) {
-      //   this.cartItems.push({
-      //     productId,
-      //     quantity: lineItem.quantity,
-      //     price: lineItem.price.value.centAmount,
-      //     totalPrice: lineItem.totalPrice.centAmount,
-      //   })
-      //   localStorage.setItem('cartItem', JSON.stringify(this.cartItems))
-      // }
-
-      this.isLoading = false
-    } catch (error) {
-      this.isLoading = false
-      console.error('Произошла ошибка при добавлении товара в корзину:', error)
-    }
-  }
-
-  async getCurrentCartState(cartId: string): Promise<{ version: number }> {
-    try {
-      const response: AxiosResponse<ApiResponse> = await api.get(
-        `${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts/${cartId}`
-      )
-      console.log(response.data.lineItems)
-      const cartItems = response.data.lineItems.map((lineItem) => ({
-        lineId: lineItem.id,
-        productId: lineItem.productId,
-        quantity: lineItem.quantity,
-        price: lineItem.price.value.centAmount,
-        totalPrice: lineItem.totalPrice.centAmount,
-        discountPrice: lineItem.price.discounted?.value.centAmount ?? 'no discount',
-      }))
-
-      // Присваиваем this.cartItems новый массив с товарами корзины
-      this.cartItems = cartItems
-      localStorage.setItem('cartItem', JSON.stringify(this.cartItems))
-      return { version: response.data.version }
-    } catch (error) {
-      console.error('Ошибка при получении текущего состояния корзины:', error)
-      throw error
-    }
-  }
-
-  async removeFromCart(productId: string) {
-    try {
-      this.isLoading = true
-
-      const cartId: string = localStorage.getItem('cartId')!
-
-      const currentCartState = await this.getCurrentCartState(cartId)
-      const items = localStorage.getItem('cartItem')!
-      const cartItemsLocal: CartItem[] = JSON.parse(items)
-      console.log(cartItemsLocal)
-      const [lineItem] = cartItemsLocal.filter((item) => item.productId === productId)
-      console.log(lineItem)
-      const requestData = {
-        version: currentCartState.version,
-        actions: [
-          {
-            action: 'removeLineItem',
-            lineItemId: lineItem.lineId,
-            // quantity: 1,
-          },
-        ],
-      }
-
-      await api.post(`${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts/${cartId}`, requestData)
-    } catch (error) {
-      this.isLoading = false
-    }
-  }
-
-  async updateCartItemQuantity(productId: string, quantity: number) {
-    try {
-      this.isLoading = true
-
-      const cartId: string = localStorage.getItem('cartId')!
-
-      const currentCartState = await this.getCurrentCartState(cartId)
-      const items = localStorage.getItem('cartItem')!
-      const cartItemsLocal: CartItem[] = JSON.parse(items)
-      console.log(cartItemsLocal)
-      const [lineItem] = cartItemsLocal.filter((item) => item.productId === productId)
-      console.log(lineItem)
-      const requestData = {
-        version: currentCartState.version,
-        actions: [
-          {
-            action: 'changeLineItemQuantity',
-            lineItemId: lineItem.lineId,
-            quantity: quantity,
-          },
-        ],
-      }
-
-      await api.post(`${commercetoolsConfig.api}/${commercetoolsConfig.projectKey}/me/carts/${cartId}`, requestData)
-    } catch (error) {
-      this.isLoading = false
-    }
-  }
-}
-
-export default CartStore
+export default observer(CartPage)
