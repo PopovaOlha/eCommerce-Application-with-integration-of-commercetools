8cc3710f027a3fb62bd56a105c90fb629ec354fa
PopovaOlha <fc.odess.10@gmail.com>
2023-09-20 17:27:48 +0300

    fix: styles

--- src/components/PromocodeList.tsx
+++ src/components/PromocodeList.tsx
@@ -3,10 +3,10 @@ const promo = ['SAVE5CART', 'SAVE10CART', 'SAVE20CART']
 const PromoCodeList: React.FC = () => {
   return (
     <div style={{ width: '100%', borderTop: '6px solid #333' }}>
-      <h2>Active promocodes:</h2>
+      <h3>Active promocodes:</h3>
       <ul style={{ color: '#333' }}>
         {promo.map((promoCode: string) => (
-          <li style={{ color: '#333', display: 'block', fontWeight: 'bold' }} key={promoCode}>
+          <li style={{ color: '#333', display: 'block', fontWeight: 'bold', fontSize: '15px' }} key={promoCode}>
             {promoCode}
           </li>
         ))}
--- src/components/SecondHeader.tsx
+++ src/components/SecondHeader.tsx
@@ -8,8 +8,8 @@ const Header = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
-    <Box sx={{ width: '100%', backgroundColor: '#fff', marginTop: '70px', borderBottom: '10px solid #333' }}>
-      <Box sx={{ justifyContent: 'space-between', display: 'flex', alignItems: 'center', p: 2 }}>
+    <Box sx={{ width: '100%', backgroundColor: '#fff', marginTop: '10px', borderBottom: '10px solid #333' }}>
+      <Box sx={{ justifyContent: 'space-between', display: 'flex', alignItems: 'center', padding: '20px' }}>
         <Box
           sx={{
             display: 'flex',
@@ -29,23 +29,20 @@ const Header = () => {
               <Link component={RouterLink} to="/delivery" sx={{ color: '#333', textDecoration: 'none' }}>
                 Shipping and payment
               </Link>
-              <Link component={RouterLink} to="/return" sx={{ color: '#333', textDecoration: 'none' }}>
-                Return
-              </Link>
               <Link component={RouterLink} to="/contacts" sx={{ color: '#333', textDecoration: 'none' }}>
                 Contacts
               </Link>
               <Link component={RouterLink} to="/catalog" sx={{ color: '#333', textDecoration: 'none' }}>
                 Catalog
               </Link>
+              <RouterLink to="/about" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+                about us
+              </RouterLink>
             </>
           )}
         </Box>
         <AdaptiveButton />
         <Box sx={{ display: 'flex', alignItems: 'center', ml: 2 }}>
-          <RouterLink to="/about" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
-            about us
-          </RouterLink>
           <PhoneIcon sx={{ color: '#333', mr: 1 }} />
           <Typography variant="body2">+123 456 789</Typography>
         </Box>
